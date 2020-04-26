ca9b940f306066908cbc99eac1ad14ae99250301
balint.kovari <balintmaci@gmail.com>
2020-04-26 12:10:16 +0200

    restructure main

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import ludopy
 import numpy as np
 
-g = ludopy.Game()
-there_is_a_winner = False
+def main():
+    g = ludopy.Game()
+    there_is_a_winner = False
 
-while not there_is_a_winner:
-    (dice, move_pieces, player_pieces, enemy_pieces, player_is_a_winner, there_is_a_winner), player_i = g.get_observation()
+    while not there_is_a_winner:
+        (dice, move_pieces, player_pieces, enemy_pieces, player_is_a_winner, there_is_a_winner), player_i = g.get_observation()
 
-    if len(move_pieces):
-        piece_to_move = move_pieces[np.random.randint(0, len(move_pieces))]
-    else:
-        piece_to_move = -1
+        if len(move_pieces) > 0:
+            piece_to_move = move_pieces[np.random.randint(0, len(move_pieces))]
+        else:
+            piece_to_move = -1
 
-    _, _, _, _, _, there_is_a_winner = g.answer_observation(piece_to_move)
+        _, _, _, _, _, there_is_a_winner = g.answer_observation(piece_to_move)
 
-print("Saving history to numpy file")
-g.save_hist(f"game_history.npy")
-print("Saving game video")
-g.save_hist_video(f"game_video.mp4")
+    print("Saving history to numpy file")
+    g.save_hist(f"game_history.npy")
+    print("Saving game video")
+    g.save_hist_video(f"game_video.mp4")
+
+if __name__ == "__main__":
+    main()
