4464bd5b07f6e7964fa068869b483879930d02c5
balint.kovari <balintmaci@gmail.com>
2020-04-26 12:14:44 +0200

    game naming

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import ludopy
 import numpy as np
 
 def main():
-    g = ludopy.Game()
+    game = ludopy.Game()
     there_is_a_winner = False
 
     while not there_is_a_winner:
-        (dice, move_pieces, player_pieces, enemy_pieces, player_is_a_winner, there_is_a_winner), player_i = g.get_observation()
+        (dice, move_pieces, player_pieces, enemy_pieces, player_is_a_winner, there_is_a_winner), player_i = game.get_observation()
 
         if len(move_pieces) > 0:
             piece_to_move = move_pieces[np.random.randint(0, len(move_pieces))]
         else:
             piece_to_move = -1
 
-        _, _, _, _, _, there_is_a_winner = g.answer_observation(piece_to_move)
+        _, _, _, _, _, there_is_a_winner = game.answer_observation(piece_to_move)
 
     print("Saving history to numpy file")
     g.save_hist(f"game_history.npy")
