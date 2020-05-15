8bd61810dfbd8ebde959a9401713f98e3a13308b
Balint Kovari <balintmaci@gmail.com>
2020-05-15 19:09:01 +0200

    added ai player in main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import ludopy
 import numpy as np
-from player.random_player import RandomPlayer as Player
+from player.random_player import RandomPlayer
+from player.ai_player import AiPlayer
 
 def main():
+    print("\n\nNew game\n----------------------\n")
     game = ludopy.Game()
     there_is_a_winner = False
-    players = []
-    for i in range(0,4):
-        players.append(Player())
+    players = create_players()
 
     while not there_is_a_winner:
         observation, player_i = game.get_observation()
@@ -16,16 +16,34 @@ def main():
         player = players[player_i]
 
         if len(move_pieces) > 0:
+            print('player: #' + str(player_i) + '\t' + str(observation))
             piece_to_move = player.select_piece_to_move(observation)
         else:
+            print('-')
             piece_to_move = -1
 
         _, _, _, _, _, there_is_a_winner = game.answer_observation(piece_to_move)
+
+        if(there_is_a_winner):
+            print('Winner is player #' + str(player_i))
+            try:
+                print(str(players[player_i].ann.get_weights()))
+            except AttributeError:
+                pass
 
     print("Saving history to numpy file")
     game.save_hist(f"game_history.npy")
     print("Saving game video")
     game.save_hist_video(f"game_video.mp4")
+
+def create_players():
+    players = []
+    num_rand = 1
+    for i in range(0,num_rand):
+        players.append(RandomPlayer())
+    for i in range(0,4-num_rand):
+        players.append(AiPlayer())
+    return players
 
 if __name__ == "__main__":
     main()
