0eb343ec10e128fed01a77ea58cf28e38eaf6da0
Balint Kovari <balintmaci@gmail.com>
2020-05-15 20:22:20 +0200

    aiplayer randomizez weights

--- player/ai_player.py
+++ player/ai_player.py
@@ -6,7 +6,10 @@ from neural_net.neural_network import neural_network
 class AiPlayer(AbstractPlayer):
     def __init__(self):
         self.number_of_inputs = 10
-        self.ann = neural_network(transfer_function = sigmoid, number_of_inputs = self.number_of_inputs, number_of_outputs = 1, number_of_hidden_layers=0, number_of_hidden_layer_neurons=0)
+        layers = [1]
+        self.ann = neural_network(transfer_function = sigmoid, number_of_inputs = self.number_of_inputs, number_of_neurons_per_layer = layers)
+        self.ann.randomize_weights(100)
+        print(self.ann.get_weights())
 
     def select_piece_to_move(self, observation):
         (dice, move_pieces, player_pieces, enemy_pieces, player_is_a_winner, there_is_a_winner) = observation
