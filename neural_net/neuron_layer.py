ba7413b8e726128709a3b14cb8e95d7872c34ba0
Balint Kovari <balintmaci@gmail.com>
2020-05-15 20:21:45 +0200

    added randomize to layer

--- neural_net/neuron_layer.py
+++ neural_net/neuron_layer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from neural_net.neuron import neuron
+from neural_net.transfer_function import sigmoid
 
 class neuron_layer():
     def __init__(self, number_of_neurons, previous_layer_number_of_neurons, transfer_function):
@@ -24,3 +25,10 @@ class neuron_layer():
         for i in range(0, len(self.neurons)):
             weights_arrays.append(self.neurons[i].get_weights())
         return weights_arrays
+
+    def randomize_weights(self, percentage):
+        for i in range(0, len(self.neurons)):
+            self.neurons[i].randomize_weights(percentage)
+
+if __name__ == "__main__":
+    my_layer = neuron_layer(5, 5, sigmoid)
