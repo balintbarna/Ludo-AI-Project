b7a5abe82ca2089bb6b389c3c9c4f9ae769c994d
Balint Kovari <balintmaci@gmail.com>
2020-05-17 15:55:34 +0200

    creating game mechanics information based on rules

--- player/ai_player.py
+++ player/ai_player.py
@@ -1,7 +1,8 @@
 import numpy as np
-from player.abstract_player import *
-from neural_net.transfer_function import sigmoid
-from neural_net.neural_network import neural_network
+from player.abstract_player import AbstractPlayer
+import player.game_mechanics as gm
+from artificial_intelligence.transfer_function import sigmoid
+from artificial_intelligence.neural_network import neural_network
 
 class AiPlayer(AbstractPlayer):
     def __init__(self):
@@ -9,18 +10,26 @@ class AiPlayer(AbstractPlayer):
         layers = [1]
         self.ann = neural_network(transfer_function = sigmoid, number_of_inputs = self.number_of_inputs, number_of_neurons_per_layer = layers)
         self.ann.randomize_weights(100)
-        print(self.ann.get_weights())
+        self.inputs = np.zeros(self.number_of_inputs)
 
     def select_piece_to_move(self, observation):
         (dice, move_pieces, player_pieces, enemy_pieces, player_is_a_winner, there_is_a_winner) = observation
-        inputs = np.zeros(self.number_of_inputs)
         max_value = 0
         max_value_index = 0
         for i in range(0, len(move_pieces)):
-            value = self.ann.calculate_outputs(inputs)[0]
-            print('value'+str(value))
+            self.make_inputs(observation, move_pieces[i])
+            value = self.ann.calculate_outputs(self.inputs)[0]
             if(value > max_value):
                 max_value = value
                 max_value_index = i
         piece_to_move = move_pieces[max_value_index]
         return piece_to_move
+
+    def make_inputs(self, observation, piece_index):
+        (dice, move_pieces, player_pieces, enemy_pieces, player_is_a_winner, there_is_a_winner) = observation
+        piece_pos = player_pieces[piece_index]
+        dest = gm.piece_destination(piece_pos, dice)
+        at_start = gm.is_start(piece_pos)
+        can_enter = gm.can_enter_game(piece_pos, dice)
+        can_finish = gm.is_goal(dest)
+
---
+++ player/game_mechanics.py
@@ -0,0 +1,74 @@
+start_pos = 0
+entered_pos = 1
+goal_pos = 59
+dice_enter_game = [6]
+star_boost_seven = [5, 18, 31, 44]
+star_boost_six = [12, 25, 38]
+safe_globes = [1, 9, 14, 22, 27, 35, 40, 48, 53]
+star_boost_eight = 51
+safe_spots = []
+quarter_map = 13
+character_pos_offset = quarter_map
+half_map = 2*quarter_map
+full_map = 2*half_map
+enemy_piece_max_reachable_pos = 53
+
+def piece_destination(piece_pos, dice):
+    if is_start(piece_pos):
+        if dice in dice_enter_game:
+            return entered_pos
+        else:
+            return start_pos
+
+    dest = piece_pos + dice
+    if dice > goal_pos:
+        dest = goal_pos - dice
+    elif dest in star_boost_six:
+        dest += 6
+    elif dest in star_boost_seven:
+        dest += 7
+    elif dest is star_boost_eight:
+        dest += 8
+    return dest
+
+def is_goal(pos):
+    return pos is goal_pos
+
+def is_start(pos):
+    return pos is start_pos
+
+def can_enter_game(pos, dice):
+    return dice in dice_enter_game and is_start(pos)
+
+def are_pieces_at_pos(pos, pieces_list):
+    return pos in pieces_list
+
+def can_build_tower(dest, own_pieces_list):
+    return are_pieces_at_pos(dest, own_pieces_list)
+
+def transform_enemy_piece(own_piece_pos, enemy_piece_pos, list_index):
+    if enemy_piece_pos > enemy_piece_max_reachable_pos: # in the finish line, cannot interact with other players
+        return 1000
+    
+    offset = list_index * character_pos_offset
+    offseted_pos = enemy_piece_pos + offset
+    if offseted_pos > enemy_piece_max_reachable_pos: # unreachable distance
+        offseted_pos -= full_map
+    while (offseted_pos - own_piece_pos) > half_map: # distances larger than a half map are hardly relevant
+        offseted_pos -= full_map
+
+def is_safe(pos):
+    return pos in safe_globes
+
+def can_kill_enemy(dest, enemy_pieces_lists):
+    for i in range(0, len(enemy_pieces_lists)):
+        enemy_pieces_list = enemy_pieces_lists[i]
+        for enemy_piece in enemy_pieces_list:
+            transformed_enemy = transform_enemy_piece(dest, enemy_piece, i)
+            if dest is transformed_enemy:
+                if enemy_piece not in safe_globes or dest is entered_pos:
+                    return True
+    return False
+
+def potential_player_progress():
+    pass
