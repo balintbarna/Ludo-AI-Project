8a5fdf85a1095b994110ba40fa513fe5bf9a1a86
Balint Kovari <balintmaci@gmail.com>
2020-05-17 18:54:09 +0200

    test out bunch of stuff in main

--- main.py
+++ main.py
@@ -5,6 +5,30 @@ from player.ai_player import AiPlayer
 import matchmaking as mm
 
 def main():
+    measure_ai_with_hand_selected_weights()
+
+def measure_ai_with_hand_selected_weights():
+    rounds = 1000
+    #            bias   enter   finish  tower   safe    kill    progress score
+    weights = [[[0,     0.9,    1,      0.2,    0.2,    0.4,    0.3]]]
+    ai = AiPlayer.fromWeights(weights)
+    wincount = mm.play_many_against_randoms(ai, rounds)
+    print("wins: " + str(wincount/rounds * 100) + "%")
+    print(str(ai.ann.get_weights()))
+
+def measure_ai_success_rate():
+    ai = AiPlayer.fromRandom()
+    wincount = mm.play_many_against_randoms(ai, 100)
+    print(wincount)
+    print(str(ai.ann.get_weights()))
+
+def ai_against_randos():
+    ai = AiPlayer.fromRandom()
+    win = mm.play_against_randoms(ai)
+    print(win)
+    print(str(ai.ann.get_weights()))
+
+def simple_game():
     players = create_players()
     winner = mm.play(players)
 
