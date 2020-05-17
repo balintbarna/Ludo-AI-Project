3ee57c71f0144b8a130bd9fa12dded9482425a53
Balint Kovari <balintmaci@gmail.com>
2020-05-17 23:49:27 +0200

    implement test program for evolutionary computing elements

--- main.py
+++ main.py
@@ -3,30 +3,64 @@ import numpy as np
 from player.random_player import RandomPlayer
 from player.ai_player import AiPlayer
 import matchmaking as mm
+import artificial_intelligence.learning as lrn
 
 def main():
     # measure_ai_with_hand_selected_weights()
-    create_ais_matchmake_with_randoms_and_measure_success()
+    # create_ais_matchmake_with_randoms_and_measure_success()
+    train_randomly_created_ais_over_generations()
     pass
+
+def train_randomly_created_ais_over_generations():
+    # first generation
+    players = create_ais_matchmake_with_randoms_and_measure_success()
+    generations = 3
+    children = 8
+    for i in range(0, generations):
+        print('----------- GENERATION ' + str(i+1) + ' -----------')
+        players = players[:2] # keep only the two winners
+        add_children(players, children)
+        reset_win_counts(players)
+        for index, player in enumerate(players):
+            matchmake_with_randoms_and_measure_success(player)
+        print("sorting list")
+        players.sort(key=lambda x: x.wincount, reverse=True)
+        for index, player in enumerate(players):
+            print("wincount " + str(player.wincount))
+    winner = players[0]
+    print('best player wins: ' + str(winner.wincount))
+    print('best player genome: ' + str(winner.ann.get_weights()))
+
+def add_children(players, children_count):
+    for _ in range(0, children_count):
+        child = lrn.create_child(players[0].ann.get_weights(), players[1].ann.get_weights())
+        lrn.introduce_mutation(child, 0.4)
+        child_player = AiPlayer.fromWeights(child)
+        players.append(child_player)
+
+def reset_win_counts(players):
+    for player in players:
+        player.wincount = 0
 
 def create_ais_matchmake_with_randoms_and_measure_success():
     ai_players = create_ai_players_with_random_weights(10)
     for index, player in enumerate(ai_players):
-        print("player " + str(index))
+        # print("player " + str(index))
         matchmake_with_randoms_and_measure_success(player)
 
     print("sorting list")
     ai_players.sort(key=lambda x: x.wincount, reverse=True)
     for index, player in enumerate(ai_players):
-        print("player " + str(index))
+        # print("player " + str(index))
         print("wincount " + str(player.wincount))
+    return ai_players
 
 def matchmake_with_randoms_and_measure_success(player):
-    rounds = 50
+    rounds = 100
     players = mm.matchmake_with_randoms([player])
     mm.play_rounds(players, rounds)
     success_percentage = 100 * player.wincount / rounds
-    print("success rate " + str(success_percentage) + "%")
+    # print("success rate " + str(success_percentage) + "%")
 
 def measure_ai_with_hand_selected_weights():
     rounds = 1000
