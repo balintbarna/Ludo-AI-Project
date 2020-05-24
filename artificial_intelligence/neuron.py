4b2e6a1db9e62a42c4cde5fc947df0e5d7c085a2
Balint Kovari <balintmaci@gmail.com>
2020-05-24 16:31:35 +0200

    neuron weights mutation

--- artificial_intelligence/neuron.py
+++ artificial_intelligence/neuron.py
@@ -34,8 +34,9 @@ class Neuron():
         return len(self._weights)
 
     def calculate_output(self, input_values):
-        bias_weight = self._weights[0]
-        neuron_weights = self._weights[1:]
+        weights = self.get_weights()
+        bias_weight = weights[0]
+        neuron_weights = weights[1:]
         assert(len(input_values) == len(neuron_weights))
         weighted_sum = np.sum(np.dot(input_values, neuron_weights)) + bias_weight
         calculated_output = self._transfer_function(weighted_sum)
@@ -48,8 +49,17 @@ class Neuron():
         return self._weights
     
     def randomize_weights(self):
-        for i in range(0, len(self)):
-            self._weights[i] = np.random.rand() * 2 - 1
+        weights = self.get_weights()
+        for i in range(0, len(weights)):
+            weights[i] = np.random.rand() * 2 - 1
+        
+    def introduce_mutation(self, max_mutation_amount):
+        weights = self.get_weights()
+        for i in range(0, len(weights)):
+            mutation = np.random.rand() * 2 - 1
+            mutation *= max_mutation_amount
+            weights[i] += mutation
+
 
 if __name__ == "__main__":
     my_neuron = Neuron.fromEmpty(5, sigmoid)
