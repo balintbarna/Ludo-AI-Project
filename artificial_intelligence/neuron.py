82db15d86d048b78ed788d676a8328d0adcd9634
Balint Kovari <balintmaci@gmail.com>
2020-05-24 15:50:00 +0200

    make vars more private

--- artificial_intelligence/neuron.py
+++ artificial_intelligence/neuron.py
@@ -7,8 +7,8 @@ except:
 
 class Neuron():
     def __init__(self, weights_np_array, transfer_function):
-        self.weights = weights_np_array
-        self.transfer_function = transfer_function
+        self._weights = weights_np_array
+        self._transfer_function = transfer_function
 
     @classmethod
     def fromEmpty(cls, number_of_inputs, transfer_function):
@@ -31,29 +31,29 @@ class Neuron():
         raise TypeError
     
     def __len__(self):
-        return len(self.weights)
+        return len(self._weights)
 
     def calculate_output(self, input_values):
-        bias_weight = self.weights[0]
-        neuron_weights = self.weights[1:]
+        bias_weight = self._weights[0]
+        neuron_weights = self._weights[1:]
         assert(len(input_values) == len(neuron_weights))
         weighted_sum = np.sum(np.dot(input_values, neuron_weights)) + bias_weight
-        calculated_output = self.transfer_function(weighted_sum)
+        calculated_output = self._transfer_function(weighted_sum)
         return calculated_output
 
     def get_weights(self):
         '''
         returns an array of doubles which are the weights belonging to the previous layer outputs
         '''
-        return self.weights
+        return self._weights
     
     def randomize_weights(self, percentage):
         fraction = percentage / 100.0
-        mask = np.random.rand(len(self.weights)) < fraction # weights length number of random doubles between 0 and 1 that are < franction; these should be changed
-        new_weights = np.random.rand(len(self.weights))
-        for i in range(0, len(self.weights)):
+        mask = np.random.rand(len(self._weights)) < fraction # weights length number of random doubles between 0 and 1 that are < franction; these should be changed
+        new_weights = np.random.rand(len(self._weights))
+        for i in range(0, len(self._weights)):
             if mask[i]:
-                self.weights[i] = new_weights[i]
+                self._weights[i] = new_weights[i]
 
 if __name__ == "__main__":
     my_neuron = Neuron.fromEmpty(5, sigmoid)
