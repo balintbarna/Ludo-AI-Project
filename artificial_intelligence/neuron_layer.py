5bf340fd92239346b30a1acb7e64e8bd5aab24c2
Balint Kovari <balintmaci@gmail.com>
2020-05-24 15:37:14 +0200

    fix up neuron layer

--- artificial_intelligence/neuron_layer.py
+++ artificial_intelligence/neuron_layer.py
@@ -1,8 +1,12 @@
 import numpy as np
-from artificial_intelligence.neuron import neuron
-from artificial_intelligence.transfer_function import sigmoid
+try:
+    from artificial_intelligence.neuron import Neuron
+    from artificial_intelligence.transfer_function import sigmoid
+except:
+    from neuron import Neuron
+    from transfer_function import sigmoid
 
-class neuron_layer():
+class NeuronLayer():
     def __init__(self, neurons_list):
         self.neurons = neurons_list
         self.calculated_outputs = np.zeros(len(neurons_list))
@@ -11,21 +15,21 @@ class neuron_layer():
     def fromEmpty(cls, number_of_neurons, previous_layer_number_of_neurons, transfer_function):
         neurons_list = []
         for _ in range(0, number_of_neurons):
-            neurons_list.append(neuron.fromEmpty(previous_layer_number_of_neurons, transfer_function))
+            neurons_list.append(Neuron.fromEmpty(previous_layer_number_of_neurons, transfer_function))
         return cls(neurons_list)
 
     @classmethod
     def fromRandom(cls, number_of_neurons, previous_layer_number_of_neurons, transfer_function):
         neurons_list = []
         for _ in range(0, number_of_neurons):
-            neurons_list.append(neuron.fromRandom(previous_layer_number_of_neurons, transfer_function))
+            neurons_list.append(Neuron.fromRandom(previous_layer_number_of_neurons, transfer_function))
         return cls(neurons_list)
 
     @classmethod
-    def fromWeights(cls, weights_list_list, transfer_function):
+    def fromWeights(cls, weights_arr_list, transfer_function):
         neurons_list = []
-        for weights_list in weights_list_list:
-            neurons_list.append(neuron.fromWeights(weights_list, transfer_function))
+        for weights_arr in weights_arr_list:
+            neurons_list.append(Neuron.fromWeights(weights_arr, transfer_function))
         return cls(neurons_list)
 
     def __len__(self):
@@ -43,14 +47,14 @@ class neuron_layer():
         '''
         returns a list of a list of doubles, that are all the weights of all the neurons in this layer
         '''
-        weights_arrays = []
+        weights_arr_list = []
         for i in range(0, len(self.neurons)):
-            weights_arrays.append(self.neurons[i].get_weights())
-        return weights_arrays
+            weights_arr_list.append(self.neurons[i].get_weights())
+        return weights_arr_list
 
     def randomize_weights(self, percentage):
         for i in range(0, len(self.neurons)):
             self.neurons[i].randomize_weights(percentage)
 
 if __name__ == "__main__":
-    my_layer = neuron_layer.fromEmpty(5, 5, sigmoid)
+    my_layer = NeuronLayer.fromEmpty(5, 5, sigmoid)
