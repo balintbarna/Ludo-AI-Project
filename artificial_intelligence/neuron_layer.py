40e9a402c5533e99c50e025f27033ebe6951514a
Balint Kovari <balintmaci@gmail.com>
2020-05-24 15:55:17 +0200

    beautify neuron layer

--- artificial_intelligence/neuron_layer.py
+++ artificial_intelligence/neuron_layer.py
@@ -8,8 +8,8 @@ except:
 
 class NeuronLayer():
     def __init__(self, neurons_list):
-        self.neurons = neurons_list
-        self.calculated_outputs = np.zeros(len(neurons_list))
+        self._neurons = neurons_list
+        self._calculated_outputs = np.zeros(len(neurons_list))
 
     @classmethod
     def fromEmpty(cls, number_of_neurons, previous_layer_number_of_neurons, transfer_function):
@@ -33,28 +33,29 @@ class NeuronLayer():
         return cls(neurons_list)
 
     def __len__(self):
-        return len(self.calculated_outputs)
+        return len(self._calculated_outputs)
 
     def calculate_outputs(self, previous_layer_values):
-        for i in range(0, len(self.calculated_outputs)):
-            self.calculated_outputs[i] = self.neurons[i].calculate_output(previous_layer_values)
-        return self.calculated_outputs
+        outputs = self.get_calculated_outputs()
+        for i in range(0, len(self)):
+            outputs[i] = self._neurons[i].calculate_output(previous_layer_values)
+        return outputs
 
     def get_calculated_outputs(self):
-        return self.calculated_outputs
+        return self._calculated_outputs
 
     def get_weights(self):
         '''
         returns a list of a list of doubles, that are all the weights of all the neurons in this layer
         '''
         weights_arr_list = []
-        for i in range(0, len(self.neurons)):
-            weights_arr_list.append(self.neurons[i].get_weights())
+        for i in range(0, len(self)):
+            weights_arr_list.append(self._neurons[i].get_weights())
         return weights_arr_list
 
     def randomize_weights(self, percentage):
-        for i in range(0, len(self.neurons)):
-            self.neurons[i].randomize_weights(percentage)
+        for neuron in self._neurons:
+            neuron.randomize_weights(percentage)
 
 if __name__ == "__main__":
     my_layer = NeuronLayer.fromEmpty(5, 5, sigmoid)
