865030cdf3ea55ffc5c66ff68014e985e2293062
Balint Kovari <balintmaci@gmail.com>
2020-05-24 16:00:23 +0200

    beautify neural network

--- artificial_intelligence/neural_network.py
+++ artificial_intelligence/neural_network.py
@@ -1,11 +1,15 @@
 import numpy as np
-from artificial_intelligence.neuron_layer import neuron_layer
-from artificial_intelligence.transfer_function import *
+try:
+    from artificial_intelligence.neuron_layer import NeuronLayer
+    from artificial_intelligence.transfer_function import *
+except:
+    from neuron_layer import NeuronLayer
+    from transfer_function import *
 
-class neural_network():
-    def __init__(self, number_of_inputs, layers_list):
-        self.layers = layers_list
-        self.number_of_inputs = number_of_inputs
+class NeuralNetwork():
+    def __init__(self, input_array, layers_list):
+        self._input = input_array
+        self._layers = layers_list
     
     @classmethod
     def fromEmpty(cls, transfer_function, number_of_inputs, number_of_neurons_per_layer):
@@ -14,9 +18,10 @@ class neural_network():
         previous_layer_neurons = number_of_inputs
         for i in range(0, number_of_layers):
             current_layer_neurons = number_of_neurons_per_layer[i]
-            layers_list.append(neuron_layer.fromEmpty(current_layer_neurons, previous_layer_neurons, transfer_function))
+            layers_list.append(NeuronLayer.fromEmpty(current_layer_neurons, previous_layer_neurons, transfer_function))
             previous_layer_neurons = current_layer_neurons
-        return cls(number_of_inputs, layers_list)
+        input_array = np.zeros(number_of_inputs)
+        return cls(input_array, layers_list)
     
     @classmethod
     def fromRandom(cls, transfer_function, number_of_inputs, number_of_neurons_per_layer):
@@ -25,22 +30,29 @@ class neural_network():
         previous_layer_neurons = number_of_inputs
         for i in range(0, number_of_layers):
             current_layer_neurons = number_of_neurons_per_layer[i]
-            layers_list.append(neuron_layer.fromRandom(current_layer_neurons, previous_layer_neurons, transfer_function))
+            layers_list.append(NeuronLayer.fromRandom(current_layer_neurons, previous_layer_neurons, transfer_function))
             previous_layer_neurons = current_layer_neurons
-        return cls(number_of_inputs, layers_list)
+        input_array = np.zeros(number_of_inputs)
+        return cls(input_array, layers_list)
     
     @classmethod
-    def fromWeights(cls, transfer_function, weights_list_list_list):
-        number_of_inputs = len(weights_list_list_list[0][0]) - 1 # number of weights in the first neuron in the first layer minus the bias weight
+    def fromWeights(cls, transfer_function, weights_list_list_arr):
+        number_of_inputs = len(weights_list_list_arr[0][0]) - 1 # number of weights in the first neuron in the first layer minus the bias weight
+        input_array = np.zeros(number_of_inputs)
         layers_list = []
-        for weights_list_list in weights_list_list_list:
-            layers_list.append(neuron_layer.fromWeights(weights_list_list, transfer_function))
-        return cls(number_of_inputs, layers_list)
+        for weights_list_list in weights_list_list_arr:
+            layers_list.append(NeuronLayer.fromWeights(weights_list_list, transfer_function))
+        return cls(input_array, layers_list)
+
+    def __len__(self):
+        return len(self._layers)
+    
+    def get_input_array(self):
+        return self._input
         
-    def calculate_outputs(self, inputs):
-        assert len(inputs) == self.number_of_inputs
-        previous_layer_outputs = inputs
-        for i in range(0, len(self.layers)):
+    def calculate_outputs(self):
+        previous_layer_outputs = self._input
+        for i in range(0, len(self)):
             previous_layer_outputs = self.layers[i].calculate_outputs(previous_layer_outputs)
         return previous_layer_outputs
     
@@ -48,13 +60,13 @@ class neural_network():
         '''
         returns a list of a list of a list of doubles, which are all the weights of all the neurons in all the layers
         '''
-        weights_arrays_of_arrays = []
-        for i in range(0, len(self.layers)):
-            weights_arrays_of_arrays.append(self.layers[i].get_weights())
-        return weights_arrays_of_arrays
+        weights_list_list_list = []
+        for i in range(0, len(self)):
+            weights_list_list_list.append(self.layers[i].get_weights())
+        return weights_list_list_list
 
     def randomize_weights(self, percentage):
-        for i in range(0, len(self.layers)):
+        for i in range(0, len(self)):
             self.layers[i].randomize_weights(percentage)
 
 if __name__ == "__main__":
@@ -62,10 +74,10 @@ if __name__ == "__main__":
     inputs = 10
     outputs = 1
     neurons_per_layer = [outputs]
-    ann = neural_network.fromEmpty(sigmoid, inputs, neurons_per_layer)
+    ann = NeuralNetwork.fromEmpty(sigmoid, inputs, neurons_per_layer)
     # few hidden layers
     inputs = 10
     outputs = 1
     neurons_per_layer = [5,5,5] # 3 hidden layers of 5 neurons
     neurons_per_layer.append(outputs)
-    ann = neural_network.fromEmpty(sigmoid, inputs, neurons_per_layer)
+    ann = NeuralNetwork.fromEmpty(sigmoid, inputs, neurons_per_layer)
